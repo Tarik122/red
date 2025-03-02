db54fda61fe02ebee7cfea9727668466dca1b97d
Tarik Bećarević <tarik.becarevic@2gimnazija.edu.ba>
2025-03-02 23:26:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -93,12 +93,12 @@ function determineNextPerson(chore) {
 // Display entries when page loads
 window.onload = function() {
     // Check if we're on a specific chore page
-    if (window.location.pathname.includes('dishwasher.html')) {
-        determineNextPerson('dishwasher');
-        displayChoreHistory('dishwasher');
-    } else if (window.location.pathname.includes('grocery.html')) {
-        determineNextPerson('grocery');
-        displayChoreHistory('grocery');
+    if (window.location.pathname.includes('mašina.html')) {
+        determineNextPerson('mašina');
+        displayChoreHistory('mašina');
+    } else if (window.location.pathname.includes('granap.html')) {
+        determineNextPerson('granap');
+        displayChoreHistory('granap');
     } else {
         // We're on the home page
         displayEntries();
